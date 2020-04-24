4015696a78ec59855a07b4e536aa2f737dc44984
Jorge Castello <jorgio.castello@gmail.com>
2020-04-24 12:31:48 -0700

    Include number of repos, search query in state

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,8 +7,10 @@ import RepoList from './components/RepoList.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      repos: []
+    this.state = {
+      numberOfRepos: 0, //This piece of state represents how many total repos are in the database
+      searchQuery: '',   //This represents the current state of the input text field
+      repos: [] //This will feature an array of objects, representative of the top 25 repos
     }
 
   }
