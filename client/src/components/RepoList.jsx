8886d13fff030baa62d90031cb36635494d53e8f
Jorge Castello <jorgio.castello@gmail.com>
2020-04-24 15:58:06 -0700

    Pass state from App to ListItem

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import RepoItem from './RepoListItem.jsx';
 
 const RepoList = (props) => (
   <div>
     <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
+    There are {props.numberOfRepos} repos.
+    <ul>
+      { props.repos.map((repo, index) => (
+        <RepoItem
+          forks={repo.forks}
+          watches={repo.watches}
+          stars={repo.stars}
+          repositoryUrl={repo.repositoryUrl}
+          repository={repo.repository}
+          usernameUrl={repo.usernameUrl}
+          username={repo.username}
+          key={repo._id}
+          count={index}
+        />
+      ))}
+    </ul>
   </div>
 )
 
