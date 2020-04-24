2c1dd2081fb4b6f8df92380faca25cc9e19365bc
Jorge Castello <jorgio.castello@gmail.com>
2020-04-24 16:02:52 -0700

    Refactor RepoListItem to create a row in table

--- client/src/components/RepoListItem.jsx
+++ client/src/components/RepoListItem.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 let RepoItem = ({ forks, watches, stars, repositoryUrl, repository, usernameUrl, username, count }) => {
   return (
-    <li>
-      <span>Repo #{count + 1}</span>
-      <a href={usernameUrl} target="_blank">{username}</a>
-      /
-      <a href={repositoryUrl} target="_blank">{repository}</a>
-      <span>{watches} watches</span>
-      <span>{stars} stars</span>
-      <span>{forks} forks</span>
-    </li>
+    <tr>
+      <td>Repo #{count + 1}</td>
+      <td>
+        <a href={usernameUrl} target="_blank">{username}</a>
+        /
+        <a href={repositoryUrl} target="_blank">{repository}</a>
+      </td>
+      <td>{watches} watches</td>
+      <td>{stars} stars</td>
+      <td>{forks} forks</td>
+    </tr>
   );
 };
 
