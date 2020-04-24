2af8bdd2ae9f46fbc710f2fe9d8e9bbaa0b9cdec
Jorge Castello <jorgio.castello@gmail.com>
2020-04-24 16:20:12 -0700

    Add forksAndStars class

--- client/src/components/RepoListItem.jsx
+++ client/src/components/RepoListItem.jsx
@@ -4,14 +4,13 @@ let RepoItem = ({ forks, watches, stars, repositoryUrl, repository, usernameUrl,
   return (
     <tr>
       <td>Repo #{count + 1}</td>
-      <td class="usernameAndRepo">
+      <td className="usernameAndRepo">
         <a href={usernameUrl} target="_blank">{username}</a>
         /
         <a href={repositoryUrl} target="_blank">{repository}</a>
       </td>
-      <td>{watches} watches</td>
-      <td>{stars} stars</td>
-      <td>{forks} forks</td>
+      <td className="forksAndStars">{forks} forks</td>
+      <td className="forksAndStars">{stars} stars</td>
     </tr>
   );
 };
